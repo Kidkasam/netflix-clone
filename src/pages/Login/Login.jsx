2c0f816ae8e099367e76098f5bf33c388362a388
kidus <kidus6135@gmail.com>
2025-09-19 18:43:46 +0300

    Add Login page with sign in/sign up form and state handling

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,53 @@
 import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { login, signup } from '../../firebase'
 
 const Login = () => {
   const [signState, setSignState] = useState("sign in")
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
+  const user_auth = async (event) => {
+    event.preventDefault()
+
+    if(signState === "sign in"){
+      await login(email, password)
+    } else {
+      await signup(name, email, password)
+    }
+  }
 
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
         <h1>{signState}</h1>
-        <form action="">
-          {signState==="sign up"? <input type="text" placeholder='name'/>:<></>}
+        <form onSubmit={user_auth}>
+          {signState === "sign up" && (
+            <input 
+              type="text" 
+              placeholder='name' 
+              value={name} 
+              onChange={(e) => setName(e.target.value)} 
+            />
+          )}
          
-          <input type="email" placeholder='email' />
-          <input type="password" placeholder='password' />
-          <button>{signState} </button>
+          <input 
+            type="email" 
+            placeholder='email'
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <input 
+            type="password" 
+            placeholder='password' 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+          <button type='submit'>{signState}</button>
+
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -25,6 +56,7 @@ const Login = () => {
             <p>need help?</p>
           </div>
         </form>
+
         <div className="form-switch">
           {signState === "sign in" ? (
             <p>
